fbd85f183efc9cb19d38a3587aa19fe929236189
Osuntuyi Amos <osuntuyiemilos@gmail.com>
2022-10-16 17:26:31 +0100

    Create Footer.jsx

---
+++ solution/src/components/Footer/Footer.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <div className="attribution hidden">
+        Challenge by
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+          Frontend Mentor
+        </a>
+        . Coded by
+        <a href="https://www.frontendmentor.io/profile/fistty" target="_blank">
+          Osuntuyi Ayomide Amos
+        </a>
+        . View other solutions
+        <a href="https://www.frontendmentor.io/profile/fistty" target="_blank">
+          here
+        </a>
+        .
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
