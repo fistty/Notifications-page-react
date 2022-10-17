4697b6e932a7d58d759d759932473d5b0e2fcd2a
Osuntuyi Amos <osuntuyiemilos@gmail.com>
2022-10-17 03:55:13 +0100

    Update Footer.jsx

--- solution/src/components/Footer/Footer.jsx
+++ solution/src/components/Footer/Footer.jsx
@@ -5,15 +5,15 @@ function Footer() {
     <footer>
       <div className="attribution hidden">
         Challenge by
-        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noopener">
           Frontend Mentor
         </a>
         . Coded by
-        <a href="https://www.frontendmentor.io/profile/fistty" target="_blank">
+        <a href="https://www.frontendmentor.io/profile/fistty" target="_blank" rel="noopener">
           Osuntuyi Ayomide Amos
         </a>
         . View other solutions
-        <a href="https://www.frontendmentor.io/profile/fistty" target="_blank">
+        <a href="https://www.frontendmentor.io/profile/fistty" target="_blank" rel="noopener">
           here
         </a>
         .
